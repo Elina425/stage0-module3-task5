ebd8065f9fc2ab70e7d046c81af4e958b6e5c7f2
Elina <elina.melkonyan.y@tumo.org>
2022-07-31 19:28:12 +0400

    5

--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -1,6 +1,11 @@
 package lang.print.gaps.task5;
 
 public class TimeConvertor {
+    public static void main(String[] args) {
+        TimeConvertor minute = new TimeConvertor();
+        minute.convert(4);
+    }
     public void convert(float minutes) {
+        float m = minutes*60;
     }
 }
