9ebb1a19ac4d68ce4f0cbb162f43c34a5b225a40
Elina <elina.melkonyan.y@tumo.org>
2022-07-31 22:56:08 +0400

    6

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -1,11 +1,9 @@
 package lang.print.gaps.task5;
 
 public class NumberDivider {
-    public static void main(String []arg){
-        NumberDivider d1 = new NumberDivider();
-        d1.divide(35);
-    }
+
     void divide(int toBeDivided) {
     int result = toBeDivided/5;
+        System.out.println(result);
     }
 }
--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -1,11 +1,9 @@
 package lang.print.gaps.task5;
 
 public class TimeConvertor {
-    public static void main(String[] args) {
-        TimeConvertor minute = new TimeConvertor();
-        minute.convert(4);
-    }
+
     public void convert(float minutes) {
         float m = minutes*60;
+        System.out.println(m);
     }
 }
